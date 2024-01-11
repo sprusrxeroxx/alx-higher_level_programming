e85519bd91af190a416a0fe4746c128f4d63a016
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-01-10 18:57:03 -0800

    TASK 0X02

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return sub(a, b)
+...........................................................
+vi 103-fast_alphabet.py
+
+#!/usr/bin/python3
+import string
+print(string.ascii_uppercase)
+
