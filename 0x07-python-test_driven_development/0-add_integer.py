11e979bb0393b6eecc5edd61e6f5199b73a6da02
ntokozo <ntokozovilakazi36@gmail.com>
2024-01-30 18:12:04 -0800

    First Files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+'''
+    a function that adds 2 integers
+'''
+def add_integer(a, b=98):
+    
+    if type(a) not in [int, float]:
+        raise TypeError ("a must be an integer")
+    
+    if type(b) not in [int, float]:
+        raise TypeError ("b must be an integer")
+
+    return int(a) + int(b)
