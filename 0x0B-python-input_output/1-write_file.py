675255fe32893a0a6c0bfafd6d4e6f8738a4a677
ntokozo <ntokozovilakazi36@gmail.com>
2024-02-06 15:29:44 -0800

    My task 2 commit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''A function to read a text file and print out to  stdout'''
+
+
+def write_file(filename="", text=""):
+    '''A representation of a file write function
+
+    Args:
+        filename: A string to capture the name of the file
+        text: The string the user wants to write
+
+    Returns:
+        Writes to a file
+    '''
+
+    with open(filename, "w", encoding='UTF-8') as fd:
+        fd.write(text)
+    return len(text)
+        
