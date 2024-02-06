9a5a4101cf9dd4cb4e322aa1efd3adfa925206b0
ntokozo <ntokozovilakazi36@gmail.com>
2024-02-06 15:07:21 -0800

    My first commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,14 @@
 '''A function to read a text file and print out to  stdout'''
 
 def read_file(filename=""):
-    '''A representation of a file read function'''
+    '''A representation of a file read function
+
+    Args:
+        filename: A string to capture the name of the file 
+
+    Returns:
+        prints Contents of file to the screen
+    '''
     with open(filename,"r", encoding='utf8') as fd:
         for line in fd:
             print(line)
