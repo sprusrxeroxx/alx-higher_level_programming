36df3e75a53dda9f1dabd0c2697121bbcf483fcc
master <ntokozovilakazi36@gmail.com>
2024-02-12 11:44:24 -0800

    /+base_class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+'''
+  This is A BASE class for the entire project
+'''
+
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        
+        if id != None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
