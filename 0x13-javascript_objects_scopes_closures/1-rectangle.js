f235385a056cd8b507075655bc6c7df4563e3503
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-09 23:55:19 +0200

    Task basis code + test

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+class Rectangle{
+    /* This is a class that defines a rectangle and its values*/
+    constructor(w, h) {
+        this.w = width;
+        this.h = height;
+      }
+}
