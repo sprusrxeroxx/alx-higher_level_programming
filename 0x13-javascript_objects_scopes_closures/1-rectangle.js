ff3e047f9e21a091f1fd5c6181f346606c985fbf
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-10 00:16:56 +0200

    quick tweak

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle{
     /* This is a class that defines a rectangle and its values*/
     constructor(w, h) {
-        this.w = width;
-        this.h = height;
+        this.witdh = w;
+        this.height = h;
       }
 }
