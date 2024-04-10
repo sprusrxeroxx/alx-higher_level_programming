250b99b5d4e8708dfb7e50ce2a4c02bb1008cf2e
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-10 03:05:19 +0200

    task 5 + test code

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+    
+    constructor (w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+          }
+    }
+    print(){
+        for(let i=0;i<this.height;i++){
+            for(let j=0; j<this.width; j++){
+              process.stdout.write("X")
+            }
+            console.log()
+          }
+    }
+    rotate(){
+        this.width = h;
+        this.height = w;
+    }
+    double(){
+        this.width *= 2;
+        this.height *= 2;
+    }
+  };
