b6ee266c913cf51ae2e5e180fad69a3c253327be
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-10 02:29:20 +0200

    mistake

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,7 +10,7 @@ module.exports = class Rectangle {
     print(){
         for (let i = 0; i < this.height; i++)
         {
-            console.log('x')*this.width;
+            console.log('x'*this.width);
         }
     }
   };
