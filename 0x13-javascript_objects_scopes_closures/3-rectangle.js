5b4ef7846693ead61cb8f88df45e5ee173a97e15
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-10 02:51:28 +0200

    logic error

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,8 +8,8 @@ module.exports = class Rectangle {
           }
     }
     print(){
-        for(let i=0;i<this.width;i++){
-            for(let j=0; j<this.height; j++){
+        for(let i=0;i<this.height;i++){
+            for(let j=0; j<this.width; j++){
               process.stdout.write("x")
             }
             console.log()
