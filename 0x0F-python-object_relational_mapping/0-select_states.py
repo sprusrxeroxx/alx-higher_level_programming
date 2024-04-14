de2a249404d3d9f465564e1c32e159bcbd87b1fc
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-15 00:06:23 +0200

    struct change

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+db = "mysql+mysqldb://root:root@localhost:3306/hbtn_0e_0_usa" #connection string to describe mysql database
+engine = create_engine(db) # creates an engine object to establish connection with Mysql database
+Base.metadata.create_all(engine) # creates a table in Mysql database (if it doesn't already exist)
+Session = sessionmaker(bind=engine) # creates session object which interacts with the database
+session = Session()
 
 
 class states(Base):
@@ -19,16 +24,10 @@ class states(Base):
         self.id = id
         self.name= name
 
-
-db = "mysql+mysqldb://root:root@localhost:3306/hbtn_0e_0_usa" #connection string to describe mysql database
-engine = create_engine(db) # creates an engine object to establish connection with Mysql database
-Base.metadata.create_all(engine) # creates a table in Mysql database (if it doesn't already exist)
-Session = sessionmaker(bind=engine) # creates session object which interacts with the database
-session = Session()
-
-
 if __name__ == '__main__':
     if len(sys.argv == 3):
       allStates= session.query(states).all()
       S = [(s.id, s.name) for s in allStates]
       print(S)
+    else:
+        pass
