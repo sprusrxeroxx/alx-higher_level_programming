734986294e2291602ddcb8cfbda2e80bd38c88e2
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-14 23:58:19 +0200

    task 1 commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+"""
+@author: NTK
+"""
+import sys
+
+from sqlalchemy import create_engine, String, Integer, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class states(Base):
+    __tablename__="states"
+    id = Column("id", Integer, nullable=False, autoincrement=True, primary_key=True)
+    name = Column("firstName", String(254))
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name= name
+
+
+db = "mysql+mysqldb://root:root@localhost:3306/hbtn_0e_0_usa" #connection string to describe mysql database
+engine = create_engine(db) # creates an engine object to establish connection with Mysql database
+Base.metadata.create_all(engine) # creates a table in Mysql database (if it doesn't already exist)
+Session = sessionmaker(bind=engine) # creates session object which interacts with the database
+session = Session()
+
+
+if __name__ == '__main__':
+    if len(sys.argv == 3):
+      allStates= session.query(states).all()
+      S = [(s.id, s.name) for s in allStates]
+      print(S)
