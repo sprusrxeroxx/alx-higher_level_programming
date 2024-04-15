7661560d7f1870901a3bc634cf018522afb6f082
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-15 04:38:12 +0200

    Smart filer task

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    
+  try:
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                        passwd=argv[2], db=argv[3]) 
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                 FROM cities JOIN states ON \
+                 cities.state_id=states.id;")
+    rows = cur.fetchall()
+    [print(i) for i in rows]
+    
+  except MySQLdb.Error as err:
+    print("Error connecting to database:", err)
+
+
+  finally:
+    if db:
+      db.close()
