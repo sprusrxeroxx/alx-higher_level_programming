d28df1b319fc4e9d31268da641572c6279e8d043
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-15 05:20:58 +0200

    print matcing cities query

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A script that takes state name as an argument \
+and displays all matching cities from the city table \
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    if len(argv) != 5:
+        print("Usage: script_name.py <username>\
+               <password> <database> <state_name>")
+        exit(1)
+        
+
+    try:
+        # a secure connection parameterization method
+        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+        cur = db.cursor()
+
+        # parameterized query with placeholder for state name
+        state_name = argv[4]
+        sql = "select name from cities where \
+                state_id = (select id from states where name = %s)"
+        cur.execute(sql, (state_name,))
+
+        rows = cur.fetchall()
+        [print(i) for i in rows]
+
+    except MySQLdb.Error as err:
+        print("Error connecting to database:", err)
+
+    finally:
+        if db:
+            db.close()
