4d99b05d8987fcd2290e550091da1bbebb5d66a2
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-27 00:58:46 +0200

    format string change

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,15 +4,13 @@ import urllib.request
 """This is a script which fetches a status for a particular url"""
 
 def fetch_status():
-    """Fetches the status of the provided URL and displays the response body."""
-
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
-    print("Body response:")
-    print(f"    - type: {type(body)}")
-    print(f"    - content: {body}")
-    print(f"    - utf8 content: {body.decode('utf-8')}")
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
 if __name__ == "__main__":
   fetch_status()
