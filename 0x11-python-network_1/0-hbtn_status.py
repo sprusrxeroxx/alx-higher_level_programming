77bacd7b9891960aae420b4e6eb4c9cd9d3f542f
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-27 01:25:32 +0200

    pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 """This is a script which fetches a status for a particular url"""
 
 def fetch_status():
-    """Fetches the status of the provided URL and displays the response body."""
+
 
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
@@ -13,6 +13,7 @@ def fetch_status():
     print(f"    - type: {type(body)}")
     print(f"    - content: {body}")
     print(f"    - utf8 content: {body.decode('utf-8')}")
+
 
 if __name__ == "__main__":
   fetch_status()
