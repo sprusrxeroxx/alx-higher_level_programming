db0fd7ebdc28c0bd4bfa3278f417acc4469e107b
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-27 00:36:22 +0200

    indentation fault

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ import urllib.request
 """This is a script which fetches a status for a particular url"""
 
 def fetch_status():
-  """Fetches the status of the provided URL and displays the response body."""
+    """Fetches the status of the provided URL and displays the response body."""
+
     url = 'https://alx-intranet.hbtn.io/status'
-  
     with urllib.request.urlopen(url) as response:
         body = response.read()
     print("Body response:")
