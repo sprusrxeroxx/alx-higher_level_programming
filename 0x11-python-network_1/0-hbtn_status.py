9e5710d4eb177ccdcdc7c35799104dc7afbe3377
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-27 01:00:23 +0200

    syntac error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,10 @@ def fetch_status():
     request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
 if __name__ == "__main__":
   fetch_status()
