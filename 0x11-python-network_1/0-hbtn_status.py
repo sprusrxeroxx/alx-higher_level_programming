5efe6150770e52445c8375b0bccfb831df751e2a
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-27 00:54:46 +0200

    stage

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,9 +10,9 @@ def fetch_status():
     with urllib.request.urlopen(url) as response:
         body = response.read()
     print("Body response:")
-    print(f"   - type: {type(body)}")
-    print(f"   - content: {body}")
-    print(f"   - utf8 content: {body.decode('utf-8')}")
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+    print(f"    - utf8 content: {body.decode('utf-8')}")
 
 if __name__ == "__main__":
   fetch_status()
