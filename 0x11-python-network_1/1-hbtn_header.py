0f7e69b6ac2b6584d752de8777df0aaf4b740247
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-27 03:17:12 +0200

    pycodesyle

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,17 +8,17 @@ import sys
 
 def get_request_id(url):
 
-  try:
-    with urllib.request.urlopen(url) as response:
-      headers = response.headers
-      request_id = headers.get("X-Request-Id")
-      if request_id:
-        print(request_id)
-      else:
-        print("X-Request-Id header not found.")
-  except urllib.error.URLError as e:
-    print(f"Error: {e}")
+    try:
+        with urllib.request.urlopen(url) as response:
+            headers = response.headers
+        request_id = headers.get("X-Request-Id")
+        if request_id:
+            print(request_id)
+        else:
+            print("X-Request-Id header not found.")
+    except urllib.error.URLError as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
-  get_request_id(sys.argv[1])
+    get_request_id(sys.argv[1])
