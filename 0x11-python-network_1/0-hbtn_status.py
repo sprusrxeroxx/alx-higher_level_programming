b4d3ea7d589bd32e04a473f2ca7583ab5759295c
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-05-01 05:33:31 +0200

    task 0 amendment

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
-
-"""This is a script which fetches a status for a particular url"""
-
-
-def fetch_status():
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-    print("Body response:")
-    print(f"    - type: {type(body)}")
-    print(f"    - content: {body}")
-    print(f"    - utf8 content: {body.decode('utf-8')}")
 
 
 if __name__ == "__main__":
-    fetch_status()
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
