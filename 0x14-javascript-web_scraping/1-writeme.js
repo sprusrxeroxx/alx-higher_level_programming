772913012ba85418087720393bba910bae3638fb
ntokozo vilakazi <ntokozovilakazi36@gmail.com>
2024-05-22 03:04:40 +0200

    feat: Add script for writing content to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// A Script that writes content to a file.
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
