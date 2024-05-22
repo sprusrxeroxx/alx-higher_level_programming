2ad8086c9e72ee76cc6caacb911a67a062dee2b1
ntokozo vilakazi <ntokozovilakazi36@gmail.com>
2024-05-22 03:38:07 +0200

    feat: Refactor file reading script to use fs module

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
-request(url, function (error, response, body) {
+request(url, function (error, _response, body) {
     if (error) {
         console.error('error:', error);
     } else {
