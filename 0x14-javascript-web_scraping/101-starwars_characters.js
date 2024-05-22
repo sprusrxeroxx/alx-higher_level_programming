db52d204bf30a07f963ca40150e5b8ff9d345ef9
ntokozo vilakazi <ntokozovilakazi36@gmail.com>
2024-05-22 04:05:58 +0200

    Update Star Wars character printing script to use updated SWAPI API URL

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,7 +5,7 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/'.concat(process.argv[2]);
 
 request(url, function (_err, _res, body) {
- const characters = JSON.parse(body).characters;
+  const characters = JSON.parse(body).characters;
 
   for (let i = 0; i < characters.length; ++i) {
     request(characters[i], function (_cErr, _cRes, cBody) {
